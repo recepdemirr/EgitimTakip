5c5fad5ec4cdf93f5ded7b0f8815f643b432cfa8
recepdemirr <recep.dmr2334@gmail.com>
2024-05-26 18:21:55 +0300

    Add project files.

---
+++ EgitimTakip.Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgitimTakip.Models
+{
+    public class Employee : BaseModel
+    {
+        public string FullName { get; set; }
+        public string TCK { get; set; }
+        public int CompanyId { get; set; }
+        public virtual Company Company { get; set; }
+    }
+}
