02dcb8b6ccfcb30961805a3792443291cf1520e8
recepdemirr <recep.dmr2334@gmail.com>
2024-06-01 20:25:17 +0300

    Layout-Giydirme Yapıldı

--- EgitimTakipWeb/Controllers/HomeController.cs
+++ EgitimTakipWeb/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 
 namespace EgitimTakipWeb.Controllers
 {
-    [Authorize]
+    
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
